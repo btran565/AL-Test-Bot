5e4b79d72ebf7c2dff069befaf58f492c0345c6f
BrandonTran15 <>
2021-12-13 17:18:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+import discord
+import requests
+import json
+
+client = discord.Client()
+key = 'K8kPDUS8O3RknkpNyBCT'
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+def get_stats(username):
+  response = requests.get("https://api.mozambiquehe.re/bridge?version=5&platform=PC&player="+ username +"&auth=K8kPDUS8O3RknkpNyBCT")
+  json_data = json.loads(response.text) #use json_data to access api's dictionaries
+  stats = "Name: " + json_data['global']['name'] +"\nRank: " + json_data['global']['rank']['rankName']
+  return stats
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$AL test'):
+    await message.channel.send('Hi! This is the AL Test Bot.')
+
+  if message.content.startswith('$AL help'):
+    await message.channel.send(
+      'This is the AL Test Bot. All commands use prefix *"$AL"*.\
+      This bot uses the ')
+
+  if message.content.startswith('$AL quote'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+  if message.content.startswith('$AL stats'):
+    username = message.content
+    username = username.split('$AL stats ')   #saves message content to string 
+    stats = get_stats(username[1])
+    await message.channel.send(stats)
+
+client.run(os.environ['TOKEN'])
