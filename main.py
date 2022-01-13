12291b781e3ce9cbc751d3fd32af924ca10017c0
Brandon Tran <btran565@gmail.com>
2022-01-13 21:30:55 +0000

    get_stats return if empty

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ def get_quote():
   return(quote)
 
 def get_stats(username):
+  if username == '':
+    return 'Please resend your command with your username at the end.\ni.e. $AL stats CheeseTerds'
   response = requests.get("https://api.mozambiquehe.re/bridge?version=5&platform=PC&player="+ username +"&auth=K8kPDUS8O3RknkpNyBCT")
   json_data = json.loads(response.text) #use json_data to access api's dictionaries
   stats = "Name: " + json_data['global']['name'] + "\nRank: " + json_data['global']['rank']['rankName'] + "\n" + json_data['global']['rank']['rankImg']
